b161242c72a3a5ab1664663ae8bdc07a17543119
James Smith <146120205+cybersmith1001@users.noreply.github.com>
2023-10-23 00:35:34 +0100

    Add files via upload

---
+++ Question to Class/script.js
@@ -0,0 +1,18 @@
+picture.addEventListener("click", (event)=>{
+    const image = document.getElementById('picture')
+    image.src = 'names.png'
+    picture.addEventListener("click", (event)=>{
+    const image = document.getElementById('picture')
+    image.src = 'number.jpeg'
+    picture.addEventListener("click", (event)=>{
+    const image = document.getElementById('picture')
+    image.src = 'date.jpeg'
+    
+    
+        setTimeout(() => {
+        image.innerHTML = image.src = 'tree.png' }, 2000)
+    
+        })
+    
+    })
+    })
